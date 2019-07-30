109a6604244309fc3c3f99af2bcf88db2558f8e7
rudreshsolank97 <rudreshsolanki101@gmail.com>
2019-07-31 01:27:10 +0530

    added lokijs & some basic loki-views

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const tmiService = require("./services/tmi-handler");
 const tmi = require("tmi.js");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const options = {
   identity: {
@@ -19,7 +22,11 @@ const tmiClient = new tmi.client(options);
 tmiClient.on("connected", tmiService.onConnectionHandler);
 tmiClient.on("message", tmiService.onMessageHandler);
 
-tmiClient.connect();
+app.get("/api/getAllChat", tmiService.showAllChat);
+app.get("/api/getAllModChat", tmiService.getAllModChat);
+app.post("/api/getAllSubChat", tmiService.getAllSubChat);
+app.get("/api/getAllStreamerChat", tmiService.getAllStreamerChat);
 
+tmiClient.connect();
 
 app.listen(process.env.PORT);
--- services/tmi-handler.js
+++ services/tmi-handler.js
@@ -1,9 +1,55 @@
+var loki = require("lokijs"),
+  db = new loki("test.json");
+
+var chat = db.addCollection("chat");
+
+var modChat = chat.addDynamicView("modChat");
+modChat.applyWhere(obj => {
+  return obj.isMod;
+});
+
+var subChat = chat.addDynamicView("subChat");
+subChat.applyWhere(obj => {
+  return obj.isSubscribed;
+});
+
+var streamerChat = chat.addDynamicView("streamerChat");
+streamerChat.applyWhere(obj => obj.from == process.env.CHANNEL_NAME);
+
 exports.onConnectionHandler = (address, port) => {
   console.log(`* Connected to ${address}:${port}`);
-}
+};
 
-exports.onMessageHandler = async (target, context, message, self) =>  {
-  const words = message.replace(/,/g, '').split(' ');
-  console.log(words);
-}
+exports.onMessageHandler = async (target, context, message, self) => {
+  const words = message.replace(/,/g, "").split(" ");
+  const subMonths = context.badges.subscriber || 0;
 
+  chat.insert({
+    fromId: context["user-id"],
+    from: context.username,
+    words: words,
+    subMonths: subMonths,
+    isMod: context.mod,
+    isSubscribed: context.subscriber
+  });
+};
+
+exports.showAllChat = (req, res) => {
+  res.status(200).json(chat.data);
+};
+
+exports.getAllModChat = (req, res) => {
+  res.status(200).json(modChat.data());
+};
+
+exports.getAllSubChat = (req, res) => {
+  const threshold = req.body.subThreshold || 0;
+  subChat.applyWhere(obj => {
+    return obj.subMonths >= threshold;
+  });
+  res.status(200).json(subChat.data());
+};
+
+exports.getAllStreamerChat = (req, res) => {
+  res.status(200).json(streamerChat.data());
+};
